39f2a7211b67d101de1e7236c3504da1315f5c19
atyenoria <eigotyoubunn30@gmail.com>
2016-03-20 00:49:57 +0900

    ,

---
+++ circle/next_old.sh
@@ -0,0 +1,11 @@
+COMMIT_ID=`git --no-pager log --pretty=oneline --abbrev-commit | head -n 1`
+echo $COMMIT_ID
+
+if [[ "$COMMIT_ID" =~ "++" ]]; then
+curl -X POST https://circleci.com/api/v1/project/HoritaWorks/laravel-prod-image/tree/master?circle-token=$CIRCLE_CI_API_KEY
+echo "build next project"
+fi
+
+
+cd laravel_app && sh release.sh $STABLE_LARABEL_PUSH_BRANCH
+
