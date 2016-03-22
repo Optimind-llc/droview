48a32a0eba0f564ba4cc311965bbdf8da5a85cc3
atyenoria <eigotyoubunn30@gmail.com>
2016-03-22 10:27:03 +0900

    circle minor

--- circle/next.sh
+++ circle/next.sh
@@ -5,6 +5,7 @@ case $GIT_NAME in
         ;;
     Merge* )
     GIT_NAME=`git --no-pager log  | head -n 3 |tail -1 | cut -d " " -f 2`
+        ;;
 esac
 
 
@@ -20,8 +21,9 @@ case $GIT_NAME in
     shiichi )
     echo $GIT_NAME
     curl -X POST https://circleci.com/api/v1/project/HoritaWorks/$1/tree/master?circle-token=807a7f50585409045679180ee0892a57f09ad346
+        ;;
     * )
     echo default
     curl -X POST https://circleci.com/api/v1/project/HoritaWorks/$1/tree/master?circle-token=${CIRCLE_CI_API_KEY}
-    ;;
+        ;;
 esac
